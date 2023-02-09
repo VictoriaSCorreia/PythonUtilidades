76122eed4f396a7c0e6054d3b2d3c7c0e11c130c
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2023-02-09 11:09:40 -0300

    UpdateToEnglish

--- Interface.py
+++ Interface.py
@@ -1,61 +1,53 @@
 from tkinter import *
-
-def entradadedados():
-    """
-    Calcular a soma do número escolhido pelo usuário com um gerado aleatoriamente
-    e se ele acertou se ela é par ou ímpar.
-    """
+def dataentry():
     from random import randint
     window2 = Toplevel()
-    window2.title('VENCEDOR')
+    window2.title('Winner')
 
-    n = int(caixa_numero.get())
-    esc = caixa_escolha.get().lower().strip()[0]
-    maquina = randint(1, 100)
-    total = n + maquina
-
-    resultado = Label(window2, text=(f'Você jogou {n} e minha jogada foi {maquina}')) 
+    n = int(number_box.get())
+    esc = choice_box.get().lower().strip()[0]
+    pc = randint(1, 100)
+    total = n + pc
+    result = Label(window2, text=(f'Your move: {n}\nMy move: {pc}')) 
     if total % 2 == 0:
-        PouI = Label(window2, text=f'{total} é PAR!')
-        if esc in 'ií':
-            vencedor = Label(window2, text='Humanos... GAMEOVER.')
+        PouI = Label(window2, text=f'{total} is an even number!')
+        if esc in 'iío' or esc.isnumeric():
+            winner = Label(window2, text='Humans... GAMEOVER.')
         else:
-            vencedor = Label(window2, text='Você leu o meu código? Eu... perdi.')
+            winner = Label(window2, text="Did you see my code? I've... lost.")
     if total % 2 != 0:
-        PouI = Label(window2, text=f'{total} é ÍMPAR!')
-        if esc == 'p':
-            vencedor = Label(window2, text='Humanos... GAMEOVER.')
+        PouI = Label(window2, text=f'{total} is an odd number!')
+        if esc in 'pe' or esc.isnumeric():
+            winner = Label(window2, text='Humans... GAMEOVER.')
         else:
-            vencedor = Label(window2, text='Você leu o meu código? Eu... perdi.')
-    resultado.grid(column=0, row=4, padx=30, pady=10)
+            winner = Label(window2, text="Did you see my code? I've... lost.")
+    result.grid(column=0, row=4, padx=30, pady=10)
     PouI.grid(column=0, row=5, padx=30, pady=10)
-    vencedor.grid(column=0, row=6, padx=30, pady=10)
-
-    voltar = Button(window2, text=' Voltar ', cursor="hand2", command=window2.destroy)
+    winner.grid(column=0, row=6, padx=30, pady=10)
+    voltar = Button(window2, text=' Back ', cursor="hand2", command=window2.destroy)
     voltar.grid(column=0, row=7, padx=30, pady=20)
 
 
 window = Tk()   # abrir e fechar a janela de exibição
 window.title('PAR ou ÍMPAR?')
-texto = f'''
-Escolha um número de 1 a 100 e eu escolherei o meu\n
-Adivinhe se a soma
-dos dois será PAR ou ÍMPAR
-'''  # alternativa melhor quando o texto é grande
-titulo = Label(window, text='')
-titulo['text'] = texto
-titulo.grid(column=0, row=0, padx=30, pady=10) 
+text = f'''
+Choose a number from 1 to 100 and I'll choose mine\n
+Guess if the sum of both is an even or odd number 
+'''  
+title = Label(window, text='')
+title['text'] = text  # alternativa melhor quando o texto é grande
+title.grid(column=0, row=0, padx=30, pady=10) 
 
-label_numero = Label(window, text='Sua jogada:')
-label_numero.grid(column=0, row=1, padx=(30, 105), pady=(20, 10))
-caixa_numero = Entry(window, width=10)
-caixa_numero.grid(column=0, row=1, padx=(105, 30), pady=(20, 10))
+number_label = Label(window, text='Your move:')
+number_label.grid(column=0, row=1, padx=(30, 105), pady=(20, 10))
+number_box = Entry(window, width=10)
+number_box.grid(column=0, row=1, padx=(105, 30), pady=(20, 10))
 
-label_escolha = Label(window, text='PAR ou ÍMPAR? ')
-label_escolha.grid(column=0, row=2, padx=(30, 105), pady=10)
-caixa_escolha = Entry(window, width=10)
-caixa_escolha.grid(column=0, row=2, padx=(105, 30), pady=10)
-botao = Button(window, text=' Resultado ', cursor="hand2", command=entradadedados)
+choice_label = Label(window, text='Even or odd? ')
+choice_label.grid(column=0, row=2, padx=(30, 105), pady=10)
+choice_box = Entry(window, width=10)
+choice_box.grid(column=0, row=2, padx=(105, 30), pady=10)
+botao = Button(window, text=' result ', cursor="hand2", command=dataentry)
 botao.grid(column=0, row=3, padx=30, pady=20)
 
 window = mainloop()   # manter a janela aberta
